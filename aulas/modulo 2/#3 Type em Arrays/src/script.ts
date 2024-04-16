973473f1036bd4157e8a3a0dc1b55b02ce53f22e
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-04-16 11:45:07 -0300

    UPDATE

--- aulas/modulo 2/#3 Type em Arrays/src/script.ts
+++ aulas/modulo 2/#3 Type em Arrays/src/script.ts
@@ -1,12 +1,7 @@
-// Aplicando tipo a váriavel no TS
-// Coloca-se sempre o tipo da váriavel, caso queira classifica-la, depois dos ":" em frente a váriavel.
+//Array de Strings
+//          TIPO
+let nomes: string[] = ['Douglas', 'Pedro' , 'Luiz', 'Fulano'];
 
-//Ex:
-//         TIPO  
-let nome: string = 'Robert';
-let idade: number = 90; //Opicional, é bom evitar. Explicação do porque, abaixo.
-let programador: boolean = true;
+let idade: number[] = [90, 15, 20, 30];
 
-//Após tipar a váriavel, caso tente modificar o tipo, vai sublinhar o erro abaixo da váriavel logo de cara indicando que há erro no código. O que é muito útil na hora de fazer a manutenção.
-
-// No caso dos números, não há necessidade de tipar, pois, o proprio TS entende que a entrada de um número já é automaticamente um "number" e segundo a documentação, recomenda-se que a gente evite fazer redundâncias, para deixar o codigo mais leve, logo, a tipagem é mais voltada a "strings" do a outros elementos mesmo. Ainda mais que há possibilidade de ter "números" como strings, então, tipar elas é algo inteligente a se fazer e evita problemas no código.
+let numeros: Array<number> = [15, 23, 90, 11]; //São a mesma coisa do só que escrito de uma forma diferente, mas ambos tem a mesma funcionalidade com a propriedade "number".
