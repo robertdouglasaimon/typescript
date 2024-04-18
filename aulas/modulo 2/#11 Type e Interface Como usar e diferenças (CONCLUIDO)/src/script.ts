7ebaffd4d8203d3f25c18c01d1a2f63fdc888fbf
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-04-18 08:37:09 -0300

    Update script.ts

--- aulas/modulo 2/#11 Type e Interface Como usar e diferenças (CONCLUIDO)/src/script.ts
+++ aulas/modulo 2/#11 Type e Interface Como usar e diferenças (CONCLUIDO)/src/script.ts
@@ -1,15 +1,3 @@
-// //Criando o proprio Tipo.
-// type Idade = string | number;
-
-// let idade: Idade = 90;
-
-// function mostrarIdade(i: Idade): Idade {
-//     return i;
-// }
-
-//Ex de duas formas de adicionar varios tipo em um tipo personalizado e usar no parametro.
-//Obs: Lembrando que ambas fazem EXATAMENTE a mesma coisa.
-
 //Forma nº 1 (Você NÃO CONSEGUE mais adicionar propriedades novas depois de criado.)
 type User1 = {
     nome: string,
